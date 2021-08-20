a7d78797649772de9de6372dd9619ba532667c5c
Ritam Chakraborty <mailbox.ritamchakraborty@gmail.com>
2021-08-20 17:24:16 +0530

    Create currency_util.dart

---
+++ lib/data/currency_util.dart
@@ -0,0 +1,53 @@
+class CurrencyUtil {
+  static Map<String, String> _allCurrencies = {
+    "AUD": "Australian Dollar",
+    "BGN": "Bulgarian Lev",
+    "BRL": "Brazilian Real",
+    "CAD": "Canadian Dollar",
+    "CHF": "Swiss Franc",
+    "CNY": "Chinese Renminbi Yuan",
+    "CZK": "Czech Koruna",
+    "DKK": "Danish Krone",
+    "EUR": "Euro",
+    "GBP": "British Pound",
+    "HKD": "Hong Kong Dollar",
+    "HRK": "Croatian Kuna",
+    "HUF": "Hungarian Forint",
+    "IDR": "Indonesian Rupiah",
+    "ILS": "Israeli New Sheqel",
+    "INR": "Indian Rupee",
+    "ISK": "Icelandic Króna",
+    "JPY": "Japanese Yen",
+    "KRW": "South Korean Won",
+    "MXN": "Mexican Peso",
+    "MYR": "Malaysian Ringgit",
+    "NOK": "Norwegian Krone",
+    "NZD": "New Zealand Dollar",
+    "PHP": "Philippine Peso",
+    "PLN": "Polish Złoty",
+    "RON": "Romanian Leu",
+    "RUB": "Russian Ruble",
+    "SEK": "Swedish Krona",
+    "SGD": "Singapore Dollar",
+    "THB": "Thai Baht",
+    "TRY": "Turkish Lira",
+    "USD": "United States Dollar",
+    "ZAR": "South African Rand"
+  };
+
+  static List<String> get codes => _allCurrencies.keys.toList();
+
+  static List<String> get currencies => _allCurrencies.values.toList();
+
+  static String? currencyFromCode(String code) => _allCurrencies[code];
+
+  static String? codeFromCurrency(String currency) {
+    for (String code in _allCurrencies.keys) {
+      if (_allCurrencies[code] == currency) {
+        return code;
+      }
+    }
+
+    return null;
+  }
+}
