0bc3382800d38a2123e7f90518ea7cc7071fef5b
Ritam Chakraborty <mailbox.ritamchakraborty@gmail.com>
2021-08-24 07:45:22 +0530

    Add flutter_bloc plugin

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:currency_converter/service/converter.dart';
 import 'package:currency_converter/views/home.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,13 +12,16 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: "Currency Converter",
-      theme: ThemeData(
-        primaryColor: Colors.red,
-        accentColor: Colors.cyanAccent,
+    return BlocProvider.value(
+      value: Converter(),
+      child: MaterialApp(
+        title: "Currency Converter",
+        theme: ThemeData(
+          primaryColor: Colors.red,
+          accentColor: Colors.cyanAccent,
+        ),
+        home: Home(),
       ),
-      home: Home(),
     );
   }
 }
